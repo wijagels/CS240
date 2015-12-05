c5b40dffa0eed393888b3990aaef07af89a2e6c4
William Jagels <will.jagels@gmail.com>
2015-12-05 14:31:58 -0500

    Pre santacon commit

--- labs/8/Main.cpp
+++ labs/8/Main.cpp
@@ -5,18 +5,21 @@
 #include <queue>
 #include <algorithm>
 #include <sstream>
+#include <fstream>
 
 class City {
     public:
         City();
-        City(std::string, std::string);
         explicit City(std::string);
         bool color = false;
         std::string name = "";
-        std::list<City> edges;
+        std::list<City*> edges;
 };
 void file(char*);
 void interactive();
+template <class Instream>
+void commands(Instream&);
+bool home(City&);
 bool path(City&, City&);
 bool auxPath(City&, City&, std::queue<City>);
 bool operator==(const City&, const City&);
@@ -24,42 +27,57 @@ std::vector<City> v;
 
 int main(int argc, char** argv) {
     if(argc == 2)
-        file(argv[2]);
+        file(argv[1]);
     else
         interactive();
 }
 
 void file(char* f) {
+    std::fstream fs;
+    fs.open(f, std::fstream::in);
+    commands(fs);
+    fs.close();
 }
 
 void interactive() {
+    commands(std::cin);
+}
+
+/*
+ * Waow, templated function, much cool, less reuse
+ */
+template <class Instream>
+void commands(Instream& in) {
     std::string t;// Smaller variable names = less bytes :)
-    std::cin >> t;
+    in >> t;
     int c = stoi(t);
+    v.reserve(c+1);
     for(;c > 0;c--) {
-        std::cin >> t;
+        in >> t;
         v.push_back(City(t));
     }
-    std::cin >> t;
+    in >> t;
     c = stoi(t);
     for(;c > 0;c--) {
-        std::cin >> t;
+        in >> t;
         auto it = std::find(v.begin(), v.end(), City(t));
-        std::cin >> t;
+        in >> t;
         auto itt = std::find(v.begin(), v.end(), City(t));
         if(it == v.end() || itt == v.end()) {
             std::cout << "Invalid" << std::endl;
             return;
         }
-        it->edges.push_back(*itt);
+        it->edges.push_back(&(*itt));
     }
-    while(std::getline(std::cin, t)) {
+    while(std::getline(in, t)) {
         std::istringstream ss(t);
         std::string s;
         ss >> s;
         if(s == "HOME") {
             ss >> s;
             std::cout << s << std::endl;
+            auto it = std::find(v.begin(), v.end(), City(s));
+            home(*it);
         }
         else if(s == "TRAVEL") {
             std::string c1, c2;
@@ -67,9 +85,17 @@ void interactive() {
             ss >> c2;
             auto it = std::find(v.begin(), v.end(), City(c1));
             auto itt = std::find(v.begin(), v.end(), City(c2));
-            path(*it, *itt);
+            if(!path(*it, *itt))
+                std::cout << "No path" << std::endl;
         }
     }
+}
+
+bool home(City& h) {
+    for(auto i : h.edges) {
+        if(path(*i, h)) return true;
+    }
+    return false;
 }
 
 bool path(City& from, City& to) {
@@ -79,16 +105,16 @@ bool path(City& from, City& to) {
     from.color = true;
     std::queue<City> q;
     q.push(from);
-    for(auto it : from.edges) {
-        std::cout << "Pathing: " << it.name << std::endl;
-        auto itt = std::find(v.begin(), v.end(), it);
-        if(auxPath(*itt, to, q)) return true;
-    }
+    for(auto it : from.edges)
+        if(auxPath(*it, to, q)) return true;
     return false;
 }
 
 bool auxPath(City& from, City& to, std::queue<City> q) {
+    if(from.color)
+        return false;
     q.push(from);
+    from.color = true;
     if(from == to) {
         while(!q.empty()) {
             std::cout << q.front().name << " ";
@@ -98,11 +124,8 @@ bool auxPath(City& from, City& to, std::queue<City> q) {
         return true;
     }
     for(auto it : from.edges) {
-        std::cout << "Pathing: " << it.name << std::endl;
-        auto itt = std::find(v.begin(), v.end(), it);
-        if(auxPath(*itt, to, q)) {
+        if(auxPath(*it, to, q))
             return true;
-        }
     }
     return false;
 }
@@ -112,11 +135,6 @@ bool operator==(const City& lhs, const City& rhs) {
 }
 
 City::City() {
-}
-
-City::City(std::string name, std::string other) {
-    this->name = name;
-    this->edges.push_back(City(other));
 }
 
 City::City(std::string name) {
